00b4d1d7a0c91bf2424a4d1573f09f6b0b809736
chavergara18 <163116947+chavergara18@users.noreply.github.com>
2025-02-11 14:03:16 +0800

    Delete task_api directory

--- task_api/admin.py
+++
@@ -1,7 +0,0 @@
-# task_api/admin.py
-from django.contrib import admin
-from .models import Task, Tag
-
-admin.site.register(Task)
-admin.site.register(Tag)
-
--- task_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TaskApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'task_api'
